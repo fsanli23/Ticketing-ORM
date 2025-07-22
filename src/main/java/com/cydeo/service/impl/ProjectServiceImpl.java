289babc3056200a60dd7273623c0409eea8797d5
fsanli23 <sanlifatih23@gmail.com>
2025-07-21 23:08:40 -0500

    generic mapper

--- src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
@@ -6,6 +6,7 @@ import com.cydeo.entity.Project;
 import com.cydeo.entity.Task;
 import com.cydeo.entity.User;
 import com.cydeo.enums.Status;
+import com.cydeo.mapper.MapperUtil;
 import com.cydeo.mapper.ProjectMapper;
 import com.cydeo.mapper.UserMapper;
 import com.cydeo.repository.ProjectRepository;
@@ -24,19 +25,19 @@ public class ProjectServiceImpl implements ProjectService {
 
     private final ProjectRepository projectRepository;
     private final ProjectMapper projectMapper;
-
     private final UserService userService;
-
     private final UserMapper userMapper;
     private final TaskService taskService;
+    private final MapperUtil mapperUtil;
 
 
-    public ProjectServiceImpl( ProjectRepository projectRepository, ProjectMapper projectMapper, UserService userService, UserMapper userMapper, TaskService taskService) {
+    public ProjectServiceImpl(ProjectRepository projectRepository, ProjectMapper projectMapper, UserService userService, UserMapper userMapper, TaskService taskService, MapperUtil mapperUtil) {
         this.projectRepository = projectRepository;
         this.projectMapper = projectMapper;
         this.userService = userService;
         this.userMapper = userMapper;
         this.taskService = taskService;
+        this.mapperUtil = mapperUtil;
     }
 
     @Override
