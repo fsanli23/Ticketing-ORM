9281c2f68fa9065ae711dbd1f4876457d80b7021
fsanli23 <sanlifatih23@gmail.com>
2025-07-31 22:34:39 -0500

    Security part 3

--- src/main/java/com/cydeo/config/SecurityConfig.java
+++ src/main/java/com/cydeo/config/SecurityConfig.java
@@ -1,16 +1,21 @@
 package com.cydeo.config;
 
 
+import com.cydeo.service.SecurityService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class SecurityConfig {
+    private final SecurityService securityService;
 
-
-/*    @Bean
+    public SecurityConfig(SecurityService securityService) {
+        this.securityService = securityService;
+    }
+    /*    @Bean
     public UserDetailsService userDetailsService(PasswordEncoder encoder) {
 
         List<UserDetails> userList = new ArrayList<>();
@@ -28,8 +33,8 @@ public class SecurityConfig {
                 .antMatchers("/project/**/**").hasAuthority("Manager")
                 .antMatchers("/task/employee/**").hasAuthority("Employee")
                 .antMatchers("/task/**").hasRole("Manager")
-               // .antMatchers("/task/**").hasAuthority("ROLE_EMPLOYEE")
-               // .antMatchers("/task/**").hasAnyRole("EMPLOYEE","ADMIN")
+                // .antMatchers("/task/**").hasAuthority("ROLE_EMPLOYEE")
+                // .antMatchers("/task/**").hasAnyRole("EMPLOYEE","ADMIN")
                 .antMatchers("/", "/login", "/fragments/**", "/images/**", "/assets/**").permitAll()
                 .anyRequest()
                 .authenticated()
@@ -40,7 +45,10 @@ public class SecurityConfig {
                 .defaultSuccessUrl("/welcome")
                 .failureUrl("/login?error=true")
                 .permitAll()
-                .and()
+                .and().logout().
+                logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login").and().rememberMe().tokenValiditySeconds(120).key("fatih").
+                userDetailsService(securityService).and()
                 .build();
     }
 
--- src/main/java/com/cydeo/entity/common/UserPrincipal.java
+++ src/main/java/com/cydeo/entity/common/UserPrincipal.java
@@ -17,6 +17,7 @@ public class UserPrincipal implements UserDetails {
     }
 
 
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         List<GrantedAuthority> authorityList = new ArrayList<>();
